5ac2295a3fbb362e5cd1379a2fea3a276e2c2d2f
Marco <marcoarodrigues29@outlook.com>
2024-11-12 20:52:42 -0300

    Commit inicial

--- database.py
+++ database.py
@@ -2,11 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./livros.db"
+DATABASE_URL = "postgresql://livraria_database_user:32IrCiXdJcsGPLNGERwUUpd5vpRECNzS@dpg-csptgo5ds78s73dc9j8g-a.oregon-postgres.render.com/livraria_database"
 
 engine = create_engine(
-    DATABASE_URL, 
-    connect_args={"check_same_thread": False}  # SQLite
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
